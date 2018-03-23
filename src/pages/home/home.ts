3946dbfe2a9c20c917fa82cba2e0c19f94ad31bb
joaopedrofn <joaopedrofn@gmail.com>
2018-03-23 11:55:51 -0300

    Google Maps loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,54 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {
+	GoogleMaps,
+	GoogleMap,
+	GoogleMapsEvent,
+	GoogleMapOptions,
+	CameraPosition,
+	MarkerOptions,
+	Marker
+} from '@ionic-native/google-maps';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+		map: GoogleMap;
+		watchId: any;
+		position: any;
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+		
+  }
+  ionViewDidLoad(){
+  	this.geolocation.getCurrentPosition().then((position) => {
+  				alert("não erro");
+  				this.position = {
+  					target: {
+  						lat: position.coords.latitude,
+  						lng: position.coords.longitude
+  					},
+  					zoom: 15,
+  					tilt: 30
+  				};
+  				this.loadMap();
+  			}).catch((err) => {
+  				alert("erro");
+  			});
+  	
+  }
+  loadMap(){
+  	let mapOptions = {
+  		camera: this.position
+  	};
+  	this.map = GoogleMaps.create('map_canvas', mapOptions);
+  	this.map.one(GoogleMapsEvent.MAP_READY).then(()=>{
+  			console.log("Map Ready!");
+  	}).catch((err) => {
+  			alert(err.message);
+  	});
   }
 
 }
